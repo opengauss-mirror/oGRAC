7f204c57b4df801ecaf6206a1a148886a2d1f8b6
leweiyang <ylw_yang@qq.com>
2026-07-10 15:59:50 +0800

    build: add incremental compile support

--- build/local_install.sh
+++ build/local_install.sh
@@ -7,6 +7,7 @@ WORK_DIR=$(cd "${CURRENT_PATH}/../../"; pwd)
 BUILD_ARGS=""
 PATCH="" # 是否在oGRAC中创建元数据
 BUILD_TYPE="release"
+INCREMENTAL_BUILD="FALSE"
 USER="ogracdba"
 COMPATIBILITY_MODE="A"
 
@@ -30,6 +31,11 @@ function oGRAC_patch() {
 }
 
 function compile() {
+    if [[ "${INCREMENTAL_BUILD}" == "TRUE" ]]; then
+        compile_incrementally
+        return
+    fi
+
     oGRAC_patch
     export local_build=true
     cd ${CODE_PATH}/build || exit 1
@@ -38,6 +44,29 @@ function compile() {
         echo "build_ograc failed."
         exit 1
     fi
+}
+
+function compile_incrementally() {
+    BUILD_TYPE=${BUILD_TYPE,,}
+    if [[ "${BUILD_TYPE}" != "debug" ]]; then
+        echo "Error: incremental compile is supported for debug builds only."
+        exit 1
+    fi
+
+    echo "Starting incremental compile..."
+    cd ${CODE_PATH}/build || exit 1
+    if ! sh Makefile.sh check-incremental-debug; then
+        echo "Please run 'sh local_install.sh compile -b debug' first to do a full debug build."
+        exit 1
+    fi
+
+    oGRAC_patch
+    export local_build=true
+    if ! sh Makefile.sh incremental-package-debug; then
+        echo "Incremental compile or package failed."
+        exit 1
+    fi
+    echo "Incremental compile and package completed."
 }
 
 function check_compatibility_mode() {
@@ -82,6 +111,7 @@ function usage() {
     echo 'Usage: sh local_install.sh compile [OPTION]'
     echo 'Options:'
     echo '  -b, --build_type=<type>       Build type, default is release.'
+    echo '  --incremental                 Incremental compile, supported for debug builds only.'
     echo '  -u, --user=<user>             User name, default is ogracdba.'
     echo '  -c, --compatibility=<dbcompatibility>        compatibility mode, default is A.'
     echo '  -h, --help                    Display thishelp and exit.'
@@ -89,7 +119,7 @@ function usage() {
 
 function parse_params()
 {
-    ARGS=$(getopt -o b:u:c: --long build_type:,user:, -n "$0" -- "$@")
+    ARGS=$(getopt -o b:u:c:h --long build_type:,user:,compatibility:,incremental,help -n "$0" -- "$@")
     if [ $? != 0 ]; then
         echo "Terminating..."
         exit 1
@@ -111,13 +141,17 @@ function parse_params()
                 check_compatibility_mode
                 shift 2
                 ;;
+            --incremental)
+                INCREMENTAL_BUILD="TRUE"
+                shift
+                ;;
             --)
                 shift
                 break
                 ;;
-            -h)
+            -h | --help)
                 usage
-                exit 1
+                exit 0
                 ;;
         esac
     done
@@ -127,7 +161,7 @@ function help() {
     echo 'Usage: sh local_install.sh [OPTION]'
     echo 'Options:'
     echo '  prepare                       Prepare compile and install dependencies.'
-    echo '  compile                       Compile oGRAC.'
+    echo '  compile [--incremental]       Compile oGRAC; --incremental reuses a debug build cache.'
     echo '  install                       Install and start oGRAC.'
     echo '  clean                         Uninstall and clean env.'
 }
@@ -137,6 +171,10 @@ function main()
     mode=$1
     shift
     parse_params "$@"
+    if [[ "${INCREMENTAL_BUILD}" == "TRUE" ]] && [[ "${mode}" != "compile" ]]; then
+        echo "Error: --incremental is supported by compile mode only."
+        exit 1
+    fi
     case $mode in
         prepare)
             prepare
